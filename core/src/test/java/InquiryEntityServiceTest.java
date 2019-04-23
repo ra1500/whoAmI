3d6a3f92afd00b9406afede1f9301b8acf46d2ac
ra1500 <nj@nj.com>
2019-04-23 21:23:20 +0900

    minor test update. no spring boot starter in child poms

--- core/src/test/java/InquiryEntityServiceTest.java
+++ core/src/test/java/InquiryEntityServiceTest.java
@@ -3,18 +3,16 @@ import core.transformers.InquiryEntityDtoTransformer;
 import db.entity.InquiryEntity;
 import db.repository.InquiryRepositoryDAO;
 import model.InquiryEntityDto;
-import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertSame;
 import static org.mockito.Mockito.*;
 
 @RunWith(SpringRunner.class)
-public class CoreTest {
+public class InquiryEntityServiceTest {
 
     private static final InquiryEntity inquiryEntity =
             new InquiryEntity(1L, "mockDesc", "mockCSV", "mockUser");
@@ -30,28 +28,25 @@ public class CoreTest {
     @Mock
     InquiryEntityDtoTransformer mockedInquiryEntityDtoTransformer;
 
-    @Before
-    public void setup() {
-        inquiryEntityDto.setDescription("mockDesc");
+    @Test
+    public void shouldProvideClientDtoWithValidDbInquiryEntity(){
+        inquiryEntityService = new InquiryEntityService(mockedInquiryRepositoryDAO, mockedInquiryEntityDtoTransformer);
+
         when(mockedInquiryRepositoryDAO.findOneByGid(anyLong())).thenReturn(inquiryEntity);
         when(mockedInquiryEntityDtoTransformer.generate(inquiryEntity)).thenReturn(inquiryEntityDto);
         when(mockedInquiryEntityDtoTransformer.generate(inquiryEntityDto)).thenReturn(inquiryEntity);
-        inquiryEntityService = new InquiryEntityService(mockedInquiryRepositoryDAO, mockedInquiryEntityDtoTransformer);
-    }
 
-    @Test
-    public void shouldProvideClientDtoWithValidDbInquiryEntity(){
+        inquiryEntityDto.setDescription("mockDesc");
+
         InquiryEntityDto validClientDto = inquiryEntityService.getInquiryEntity(1L);
         assertEquals (inquiryEntityDto.getDescription(), validClientDto.getDescription());
-        // verify InquiryRepository.findOneById invoked
         verify(mockedInquiryRepositoryDAO).findOneByGid(1L);
-        // verify DtoTransformer.getInquiryEntity invoked
         verify(mockedInquiryEntityDtoTransformer).generate(inquiryEntity);
     }
 
     @Test
     public void shouldProvideDbInquiryEntityWithValidClientDto(){
-
+        //TBD
     }
 
 }
